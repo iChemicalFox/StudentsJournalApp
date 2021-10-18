8e26d15a228891348cab9d60ebbfa68db8dbdc32
Aleksei Slonevskii <a.slonevskiy@corp.vk.com>
2021-10-18 17:55:58 +0300

    create project

---
+++ StudentsJournalApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentsJournalApp
+//
+//  Created by Алексей Слоневский on 18.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
